42ad626005da0667bc147f1a647be11276021371
Erik Behrends <behrends@dhbw-loerrach.de>
2020-11-05 12:04:19 +0000

    replit/database hinzugefügt

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const { readName } = require('./util/input');
+const Database = require('@replit/database');
 
 const name = readName();
 console.log(`Hallo, ${name}!`);
+
+const db = new Database()
+console.log(`NAME ${name} in der Datenbank speichern`);
+db.set('NAME', name);
+
+console.log('NAME aus der Datenbank lesen');
+db.get('NAME').then(value => {
+  console.log(`Der Name aus der Datenbank: ${value}`)
+});
+
+// TODO: Namen als Liste speichern
